5e83af2fae0aa514461176d04752a78b238b355c
Jonathan Lauxen Romano <jjonathanromano@gmail.com>
2022-04-12 23:03:24 -0300

    ajustando o css e adcionando conteudo na home

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ return (
 			<link rel="icon" href="/favicon.ico" />
 		</Head>
 
-		<main>
+		<main className={styles.container}>
 	
 			<div className={styles.main}>
 				<div>
@@ -75,18 +75,34 @@ return (
 				</div>
 
 				<div className={styles.textBox}>
-				Nós somos uma iniciativa nova que consiste em um grupo de profissionais qualificados que já se provaram no mercado. Vendo a dificuldade de empresas de pequeno e médio porte de entrar no mundo digital de maneira consiga, decidimos unir forças para oferecer um serviço que traga resultados satisfatórios e que tenham um preço justo e acessível. 
-				<br/>
-				Acreditamos que não é sobre estar na internet, mas sim, sobre destacar o seu negócio no mundo digital!
+					Nós somos uma iniciativa nova que consiste em um grupo de profissionais qualificados que já se provaram no mercado. Vendo a dificuldade de empresas de pequeno e médio porte de entrar no mundo digital, decidimos unir forças para oferecer um serviço que traga resultados satisfatórios e que tenham um preço justo e acessível.
+					<p>
+						"Acreditamos que não é sobre estar na internet, mas sim sobre destacar o seu negócio no mundo digital!"
+					</p>
+				</div>
+
+				<div className={styles.textBox2}>
+					
+				<div>
+						<h2>
+							Missão: 
+						</h2>
+						Incluir a sua empresa no mundo digital, oferecendo soluções de comunicação e gerando conteúdos com estratégias assertivas. Sempre alcançando resultados.					</div>
+					<div>
+						<h2>
+							Visão: 
+						</h2>
+						Temos como objetivo conquistar o mundo com nossas soluções digitais, desenvolvendo estratégias/projetos que integrem a sua marca ao seu consumidor.
+					</div>
+					<div>
+						<h2>
+							Valores:
+						</h2>
+						Comunicação; Urgência; Paixão; Resultados; Inovação; Pró-atividade; Criatividade; Trabalho em equipe e Determinação.
+					</div>
 				</div>
 
 			</div>
-
-			<Link href="/#">
-				<a className={styles.menuItem}>
-					Blog
-				</a>
-			</Link>
 
 		</main>
 
